903583a5a70ab97a1f81b73a2683241f889081cd
Regan Willis <reganwillis@outlook.com>
2021-11-29 12:42:03 -0600

    basic program framework in place

--- WebScraperToNotionDatabase.py
+++ WebScraperToNotionDatabase.py
@@ -4,8 +4,6 @@ import configparser
 from rich import print
 from bs4 import BeautifulSoup
 
-scraped_text = ""
-
 print('Welcome to the Web Scraper to Notion Database App!')
 
 # read database variables from configuration file
@@ -18,31 +16,21 @@ try:
 except:
     sys.exit('ERROR: unable to read configuration file \'' + config_file + '\'')
 
-# input (required) - secret key, database key, URL
-# input (optional) - name of new database, option to save web-scraped data to local text file
-save_locally = False  # TODO: change default to false
-
 # user can scrape as many URLs as they want
 while True:
     url = input("Enter URL or type \'EXIT\' to quit: ")
+    scraped_text = ""
 
     # allow user to quit
     if url.upper() == 'EXIT':
         break
 
+    # scrape HTML of URL provided by user
     try:
-        # scrape HTML of URL provided by user
         html = requests.get(url)
-    except:
-        print('ERROR: unable scrape HTML from \'' + url + '\'')
-        continue
-
-    try:
-        # parse HTML
         soup = BeautifulSoup(html.content, "html.parser")
 
-        # TODO: create folder for output
-        # TODO: option to save just HTML in output file
+        scraped_text = ""
 
         # TODO: grab title from html
         title = "test title"
@@ -50,7 +38,12 @@ while True:
         # default - raw text
         raw_text = soup.get_text()
         scraped_text = raw_text
+    except:
+        print('ERROR: unable scrape HTML from \'' + url + '\'')
+        continue
 
+    # format HTML for readability
+    try:
         # TODO: spread text out by tag for readability
         #strs = [text for text in soup.stripped_strings]
         #print(strs)
@@ -58,13 +51,29 @@ while True:
         # TODO: medium compatable
         # if https://medium.com/ is in url
         # convert code chunks to code block
+        pass
+    except:
+        print('error formatting HTML')
+
+    # save scraped HTML locally
+    try:
+        # input (optional) - name of new database, option to save web-scraped data to local text file
+        save_locally = False  # TODO: change default to false
+        local_location = ''
+
+        # TODO: create folder for output
+        # TODO: option to save just HTML in output file
 
         # TODO: save to local text file
         if save_locally:
 
             with open('webscraped_html.txt', 'w') as file:
                 file.write(scraped_text)
+    except:
+        print('ERROR: unable to save HTML locally to \'' + local_location + '\'')
 
+    # save scraped HTML to notion database
+    try:
         # handle limit by splitting text into multiple blocks
         print(len(scraped_text))
         max_block_length = 2000 - 1
@@ -134,5 +143,6 @@ while True:
         new_page_id = response_json.get("id")
         print(new_page_id)
     except:
-        print('unknown error')
+        print('error saving to Notion')
+
 sys.exit('Thanks for using the Web Scraper to Notion Database App. Goodbye!')
