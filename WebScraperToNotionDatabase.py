ae168ff75214562b29d66cde31885d3e1c6088d5
Regan Willis <reganwillis@outlook.com>
2021-11-30 12:23:46 -0600

    fixed error in printing all blocks, terminal print format, title, article, and a tag

--- WebScraperToNotionDatabase.py
+++ WebScraperToNotionDatabase.py
@@ -4,22 +4,36 @@ import configparser
 from rich import print
 from bs4 import BeautifulSoup
 
-print('Welcome to the Web Scraper to Notion Database App!')
+print('[underline yellow]Welcome to the Web Scraper to Notion Database App!')
+
+# print formatted progress and error messages
+def print_formatted(string, message_type=('progress', 'error')):
+
+    if message_type == 'progress':
+        print('[blue]' + string + '...[/blue]')
+
+    if message_type == 'error':
+        print('[red][bold]ERROR:[/bold] ' + string + '[/red]')
 
 # read database variables from configuration file
+# TODO: add any default variables to configuration file
 try:
+    print_formatted('Reading configuration file', 'progress')
+
     config_file = 'config.txt'
     config = configparser.ConfigParser()
     config.read(config_file)
     auth = config['notionAPI']['INTEGRATION_AUTH']
     db_id = config['notionAPI']['DATABASE_ID']
 except:
-    sys.exit('ERROR: unable to read configuration file \'' + config_file + '\'')
+    print_formatted('unable to read configuration file \'' + config_file + '\'', 'error')
+    print_formatted('Exiting program', 'progress')
+    sys.exit()
 
 # user can scrape as many URLs as they want
 while True:
     url = input("Enter URL or type \'EXIT\' to quit: ")
-    scraped_text = ""
+    scraped_text = []
 
     # allow user to quit
     if url.upper() == 'EXIT':
@@ -27,33 +41,57 @@ while True:
 
     # scrape HTML of URL provided by user
     try:
+        print_formatted('Scraping HTML from \'' + url + '\'', 'progress')
+
         html = requests.get(url)
         soup = BeautifulSoup(html.content, "html.parser")
 
-        scraped_text = ""
-
-        # TODO: grab title from html
-        title = "test title"
-
-        # default - raw text
-        raw_text = soup.get_text()
-        scraped_text = raw_text
+        # default values
+        title = "New Entry Saved from Web Scraper"
+        scraped_text.append(soup.get_text())
     except:
-        print('ERROR: unable scrape HTML from \'' + url + '\'')
+        print_formatted('unable scrape HTML from \'' + url + '\'', 'error')
         continue
 
     # format HTML for readability
     try:
+        print_formatted('Formatting HTML for readability', 'progress')
+
+        scraped_text = []
+
+        # grab title from head
+        title = soup.title.string
+
+        # only work with text in article tag
+        article = soup.article
+        #print(article.get_text())
+        scraped_text.append(article.get_text())
         # TODO: spread text out by tag for readability
         #strs = [text for text in soup.stripped_strings]
         #print(strs)
+        #for i in soup.prettify().split('\n'):
+        #    print(i)
 
         # TODO: medium compatable
         # if https://medium.com/ is in url
-        # convert code chunks to code block
-        pass
-    except:
-        print('error formatting HTML')
+        
+        # extract all links
+        links = []
+        for link in article.find_all('a'):
+            stripped_link = str(link.string) + '\t' + link.get('href')
+            links.append(stripped_link)
+            #print(stripped_link)
+            scraped_text.append(stripped_link)
+        # add them as notion inline links
+        # add separate link section at the bottom
+
+        # extract images
+
+        # turn gists into code blocks
+    except Exception as e:
+        print_formatted('unable to format HTML', 'error')
+        print(e)
+        print_formatted('Continuing with raw text', 'progress')
 
     # save scraped HTML locally
     try:
@@ -70,28 +108,35 @@ while True:
             with open('webscraped_html.txt', 'w') as file:
                 file.write(scraped_text)
     except:
-        print('ERROR: unable to save HTML locally to \'' + local_location + '\'')
+        print_formatted('unable to save HTML locally to \'' + local_location + '\'', 'error')
 
     # save scraped HTML to notion database
     try:
         # handle limit by splitting text into multiple blocks
-        print(len(scraped_text))
+        #print(len(scraped_text))
         max_block_length = 2000 - 1
         blocks = []
-        if len(scraped_text) > max_block_length:
-            length_count = 0
-            block = ""
-            for char in scraped_text:
-                block = block + char
-                length_count = length_count + 1
-                if length_count == max_block_length:
-                    blocks.append(block)
-                    block = ""
-                    length_count = 0
+
+        for item in scraped_text:
+            if len(item) > max_block_length:
+                length_count = 0
+                block = ""
+
+                for char in item:
+                    block = block + char
+                    length_count = length_count + 1
+
+                    if length_count == max_block_length:
+                        blocks.append(block)
+                        #print(block + '\n')
+                        block = ""
+                        length_count = 0
+                blocks.append(block)
+            else:
+                blocks.append(item)
 
         children = []
         for block in blocks:
-            print(block + '\n')
             new_block = {
                 "paragraph": {
                     "text": [
@@ -105,7 +150,7 @@ while True:
             }
             children.append(new_block)
 
-        # TODO: save web-scraped data to notion
+        # save to notion
         notion_url = "https://api.notion.com/v1/pages/"
         # TODO: create new database if none exists
         # TODO: link property with link to article
@@ -121,7 +166,6 @@ while True:
             }
         }
 
-        # post scraped data to notion database
         response = requests.post(notion_url,
             headers = {
                 "Content-Type": "application/json",
@@ -129,20 +173,24 @@ while True:
                 "Authorization": "Bearer " + auth
             },
             json = {
-            "parent": {
-                "database_id": db_id
-            },
-            "properties": properties,
-            "children": children
+                "parent": {
+                    "database_id": db_id
+                },
+                "properties": properties,
+                "children": children
             }
         )
 
-        # get id of new page
-        response_json = response.json()
-        print(response_json)
-        new_page_id = response_json.get("id")
-        print(new_page_id)
-    except:
-        print('error saving to Notion')
+        # print response to user
+        #print(response.json())
 
-sys.exit('Thanks for using the Web Scraper to Notion Database App. Goodbye!')
+        # get id of new page
+        #response_json = response.json()
+        #print(response_json)
+        #new_page_id = response_json.get("id")
+        #print(new_page_id)
+    except:
+        print_formatted('unable to save to Notion', 'error')
+
+print('[yellow]Thanks for using the Web Scraper to Notion Database App. Goodbye!')
+sys.exit()
